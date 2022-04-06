4b0941aae901b1cff5c81f9246695b425d11af22
luissssssssssssssxd <98342890+luissssssssssssssxd@users.noreply.github.com>
2022-04-06 18:06:09 -0400

    ValidateUpdate

--- node/src/controllers/UserController.js
+++ node/src/controllers/UserController.js
@@ -26,25 +26,32 @@ module.exports = {
 
     },
     async update(req,res){
-
+        const id = req.params.id;
         const {name,password,email} = req.body;
 
         const {user_id} = req.params;
+        try {
+            const user = await User.findByPk(id);
+            if(user){
+                await User.update({ name,password,email}),{where:{id}};
 
-      
-
-        await User.update({
-            name,password,email
-        },{
-            where:{
-                id:user_id
+                return res.status(200).json({
+                    status:1,
+                    message:"Usuario Actualizado correctamente",
+                });
+            }else{
+                return res.status(400).json({
+                    status:0,
+                    message:'Usuario no encontrado'
+                });
             }
-        });
-      
-        return res.status(200).send({
-            status:1,
-            message:"Usuario actualizado correctamente",
-        })
+        } catch (error) {
+            return res.status(400).json({
+                status: 0,
+                message: 'Error al actualizar Usuario!' + error
+            });
+        }
+    
 
     },
     async delete (req,res){
